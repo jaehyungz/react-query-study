d87360197dc71c5f9bc1ab3ed8217db19f7b446e
최재형/개발/SKDND <20240148@skdnd.com>
2024-08-29 17:50:17 +0900

    리팩토링전 구현

--- src/app/components/Calendar/Calendar.tsx
+++ src/app/components/Calendar/Calendar.tsx
@@ -1,32 +1,132 @@
 "use client";
-import React, { useMemo, useState } from "react";
+import React, {
+  memo,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 
 import "./style.css";
 import dayjs from "dayjs";
-import { tableData } from "@/app/utils/dummy";
-import { format } from "path";
+import { tableData, tableType } from "@/app/utils/dummy";
+import { usePathname, useRouter } from "next/navigation";
 interface Props {
   startedAt: string;
   endedAt: string;
 }
 
+const getDateRange = (startDate: string, endDate: string) => {
+  const start = dayjs(startDate);
+  const end = dayjs(endDate);
+
+  let dateArray = [];
+
+  let currentDate = start;
+  while (currentDate.isBefore(end) || currentDate.isSame(end)) {
+    dateArray.push(currentDate.format("YYYY-MM-DD"));
+    currentDate = currentDate.add(1, "day");
+  }
+
+  return dateArray;
+};
+
 function Calendar(props: Props) {
+  console.log("!");
   const {} = props;
+  const scrollDivRef = useRef<HTMLDivElement | null>(null);
+  const router = useRouter();
+  const pathname = usePathname();
 
-  const getDateRange = (startDate: string, endDate: string) => {
-    const start = dayjs(startDate);
-    const end = dayjs(endDate);
+  const [hoverKeyIndex, setHoverKeyIndex] = useState("");
+  const [currentRowIdx, setCurrentRowIdx] = useState<number | undefined>(
+    undefined
+  );
 
-    let dateArray = [];
+  const getSearchDays = useCallback(() => {
+    return {
+      searchDays: getDateRange(props.startedAt, props.endedAt),
+    };
+  }, []);
 
-    let currentDate = start;
-    while (currentDate.isBefore(end) || currentDate.isSame(end)) {
-      dateArray.push(currentDate.format("YYYY-MM-DD"));
-      currentDate = currentDate.add(1, "day");
-    }
+  const { searchDays } = getSearchDays();
 
-    return dateArray;
+  // const [selectDays,]
+
+  // const itemRef = useRef<any>();
+
+  // const useRefs = () => {
+  //   const refsByKey = useRef<Record<string, HTMLElement | null>>({});
+
+  //   const setRef = (element: HTMLElement | null, key: string) => {
+  //     refsByKey.current[key] = element;
+  //   };
+
+  //   return { refsByKey: refsByKey.current, setRef };
+  // };
+
+  // const { refsByKey, setRef } = useRefs();
+  // const refs = Object.values(refsByKey).filter(Boolean);
+
+  const [isReady, setIsReady] = useState(true);
+
+  // 글로벌 유틸함수
+
+  const debounce = <T extends (...args: any[]) => any>(
+    fn: T,
+    delay: number
+  ) => {
+    let timeout: ReturnType<typeof setTimeout>;
+
+    return (...args: Parameters<T>): ReturnType<T> => {
+      let result: any;
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        result = fn(...args);
+      }, delay);
+      return result;
+    };
   };
+
+  const getFirstDays = () => {
+    const res = getDateRange(props.startedAt, props.endedAt);
+
+    const firstDays = res.filter((item) => dayjs(item).format("DD") === "01");
+
+    return firstDays;
+  };
+
+  // 글로벌 유틸함수 -----------------------------
+
+  const removeHoverBox = (target: "all" | "row" | "col") => {
+    const hoverBoxes = {
+      col: document.querySelector(".col-visible"),
+      row: document.querySelector(".row-visible"),
+    };
+
+    const setClass = (box: Element | null) => {
+      box?.setAttribute("class", "col-hover-box");
+    };
+
+    if (target === "row") setCurrentRowIdx(undefined);
+
+    if (target === "all") {
+      Object.values(hoverBoxes).forEach(setClass);
+    } else {
+      setClass(hoverBoxes[target]);
+    }
+  };
+
+  const fullWidth = useMemo(() => {
+    // console.log(a);
+    const res = 40 * searchDays.length + 2 * (searchDays.length - 1);
+
+    return res;
+  }, [searchDays.length]);
+
+  const currentWidth = useMemo(() => {}, []);
 
   const removeClass = (className: string) => {
     const activeContract = document.querySelectorAll(".active-contract");
@@ -40,125 +140,301 @@ function Calendar(props: Props) {
     });
   };
 
+  const handleClickDate = (date: string) => () => {
+    removeHoverBox("col");
+
+    const hoverBox = document.getElementById(`${date}-0`)?.children.item(0);
+    const dateTooltip = hoverBox?.children.item(0);
+
+    hoverBox?.setAttribute("class", "col-hover-box col-visible");
+    hoverBox?.setAttribute(
+      "style",
+      `height: ${40 * tableData.length + 8 * (tableData.length - 1)}px;`
+    );
+
+    if (date === props.startedAt)
+      dateTooltip?.setAttribute(
+        "style",
+        `transform: translateX(0px); left:0px;`
+      );
+
+    if (date === props.endedAt)
+      dateTooltip?.setAttribute(
+        "style",
+        `transform: translateX(0px); left:unset; right:0px;`
+      );
+
+    removeClass("active-contract");
+    return false;
+  };
+
+  const handleClickUnit = (idx: number) => () => {
+    //전에 클릭한 호실을 다시 클릭할때
+    if (currentRowIdx === idx) return removeHoverBox("row");
+
+    removeHoverBox("row");
+
+    setCurrentRowIdx(idx);
+
+    const dataLength = searchDays.length;
+
+    const hoverBox = document
+      .getElementById(`${props.startedAt}-${idx}`)
+      ?.children.item(1);
+    // asd(dataLength);
+
+    hoverBox?.setAttribute("class", "row-hover-box row-visible");
+    hoverBox?.setAttribute(
+      "style",
+      // `width:${40 * dataLength + 2 * (dataLength - 1)}px`
+      `width:${fullWidth}px`
+    );
+  };
+
+  // useMemo(() => {
+  //   const select = document.getElementById(`${hoverKeyIndex}-${0}`)?.children;
+
+  //   select?.item(0)?.setAttribute("class", "hover-box visible");
+
+  //   // select && select.style
+  //   // const mask = document.createElement("div");
+  //   // const appendMask = select?.appendChild(mask);
+
+  //   // appendMask?.classList.add("hover-box");
+
+  //   // mask.style.height = `${40 * tableData.length + 8 * tableData.length - 2}px`;
+
+  //   // console.log(hoverKeyIndex.length ? "없음" : hoverKeyIndex);
+  // }, [hoverKeyIndex]);
+
+  // const handleOnMouseOver = (isIclude: boolean, date: string) => {
+  //   console.log("over");
+  // console.log(hoverKeyIndex, "over");
+  // if (hoverKeyIndex.length) return;
+  // setHoverKeyIndex(date);
+
+  // const select = document.getElementById(`${date}-${0}`);
+
+  // const mask = document.createElement("div");
+  // const appendMask = select?.appendChild(mask);
+
+  // appendMask?.classList.add("hover-box");
+
+  // mask.style.height = `${40 * tableData.length + 8 * tableData.length - 2}px`;
+  // };
+  // const handleOnMouseOut = (date: string) => {
+  //   console.log("out");
+  // console.log(hoverKeyIndex, "out");
+
+  // setHoverKeyIndex('')
+  // const element = document.querySelector(".hover-box");
+  // element?.remove();
+  // element.forEach((el) => el.remove());
+  // setHoverKeyIndex("");
+  // element.forEach((val) => val.remove());
+  // };
+
+  // const debouncedOnMouseOver = debounce<typeof handleOnMouseOver>(
+  //   handleOnMouseOver,
+  //   0
+  // );
+  // const debouncedOnMouseOut = debounce<typeof handleOnMouseOut>(
+  //   handleOnMouseOut,
+  //   0
+  // );
+
   return (
-    <div className="calendar-container">
-      <div className="unit">
-        {tableData.map((item) => {
-          return (
-            <div key={item.unitId}>
-              <span>{item.unitId}</span>
-              <strong>{item.unitName}</strong>
-            </div>
-          );
-        })}
-      </div>
-      <div className="date">
-        {tableData.map((table, tableIdx) => {
-          return (
-            <div className="date-container" key={tableIdx}>
-              {getDateRange(props.startedAt, props.endedAt).map((date, idx) => {
-                let temp: string[] = [];
-                let firstDate: string[] = [];
-                let currentDate: string[] = [];
+    <>
+      {/* <button onClick={handleScroll}>스크롤 이동</button> */}
 
-                table.contractDates.map((contractDate) => {
-                  const res = getDateRange(
-                    contractDate.startedAt,
-                    contractDate.endedAt
-                  );
+      <div className="calendar-container">
+        <div className="unit">
+          <div />
+          {tableData.map((item, idx) => {
+            return (
+              <div key={item.id} onClick={handleClickUnit(idx)}>
+                <strong>{item.unitName}</strong>
+              </div>
+            );
+          })}
+        </div>
+        <div className="date" ref={scrollDivRef}>
+          <div className="date-row">
+            {/* {getDateRange(props.startedAt, props.endedAt).map((item, idx) => { */}
+            {searchDays.map((item, idx) => {
+              const firstDays = getFirstDays();
 
-                  firstDate = [...firstDate, res[0]];
-                  temp = [...temp, ...res];
-                });
+              let isFirst = idx === 0 ? true : firstDays.includes(item);
+              return (
+                <div
+                  key={item}
+                  className={`date-item date-view ${isFirst ? "first" : ""}`}
+                  onClick={handleClickDate(item)}
+                >
+                  {isFirst ? (
+                    <strong>{dayjs(item).format("M")}월</strong>
+                  ) : (
+                    <></>
+                  )}
 
-                let isIclude = temp.includes(date);
-                let isFirst = firstDate.includes(date);
-                let isActiveContract = currentDate.includes(date);
+                  <span>{dayjs(item).format("DD")}</span>
+                </div>
+              );
+            })}
+          </div>
+          {tableData.map((table, tableIdx) => {
+            return (
+              <div className="date-row" key={table.id}>
+                {searchDays.map((date, rowIdx) => {
+                  let temp: string[] = [];
+                  let firstDate: string[] = [];
+                  let cleanupdays: string[] = [];
+                  let currentDate: string[] = [];
 
-                return (
-                  <div
-                    key={date}
-                    id={`${date}-${tableIdx}`}
-                    className={`date-item
+                  table.contractDates.map((contractDate) => {
+                    const res = getDateRange(
+                      contractDate.startedAt,
+                      contractDate.endedAt
+                    );
+
+                    const lastday = res[res.length - 1];
+                    const result = getDateRange(
+                      dayjs(lastday).add(1, "day").format("YYYY-MM-DD"),
+                      dayjs(lastday).add(2, "day").format("YYYY-MM-DD")
+                    );
+
+                    firstDate = [...firstDate, res[0]];
+                    cleanupdays = [...cleanupdays, ...result];
+                    temp = [...temp, ...res];
+                  });
+
+                  // 계약일에 포함되어있는지
+                  let isIclude = temp.includes(date);
+                  // 계약일중 첫날인지
+                  let isFirst = firstDate.includes(date);
+                  // 지금 클릭한 날짜가 계약일에 포함되어있는지
+                  let isActiveContract = currentDate.includes(date);
+                  // 계약만료후 이틀의 공실인지
+                  let isCleanupday = cleanupdays.includes(date);
+
+                  return (
+                    <div
+                      key={`${date}-${tableIdx}`}
+                      id={`${date}-${tableIdx}`}
+                      className={`date-item
                         ${
-                          isActiveContract
-                            ? "active-contract"
-                            : isIclude
-                            ? isFirst
-                              ? "start"
-                              : "color"
+                          isReady
+                            ? isActiveContract
+                              ? "active-contract"
+                              : isIclude
+                              ? isFirst
+                                ? "start"
+                                : "color"
+                              : isCleanupday
+                              ? "cleanup-day"
+                              : ""
                             : ""
                         }
                     `}
-                    onClick={() => {
-                      removeClass("active-contract");
-                      const findIndex = table.contractDates.findIndex((a) =>
-                        getDateRange(a.startedAt, a.endedAt).includes(date)
-                      );
-
-                      if (findIndex >= 0) {
-                        const res = getDateRange(
-                          table.contractDates[findIndex].startedAt,
-                          table.contractDates[findIndex].endedAt
+                      onClick={(e) => {
+                        removeHoverBox("all");
+                        removeClass("active-contract");
+                        const findIndex = table.contractDates.findIndex(
+                          (contractDate) =>
+                            getDateRange(
+                              contractDate.startedAt,
+                              contractDate.endedAt
+                            ).includes(date)
                         );
-                        if (res.includes(date)) {
-                          res.map((item, idx) => {
-                            if (idx === 0) {
+
+                        if (findIndex >= 0) {
+                          const res = getDateRange(
+                            table.contractDates[findIndex].startedAt,
+                            table.contractDates[findIndex].endedAt
+                          );
+                          if (res.includes(date)) {
+                            res.map((item, idx) => {
+                              if (idx === 0) {
+                                const element = document.getElementById(
+                                  `${item}-${tableIdx}`
+                                );
+                                const box = document.createElement("div");
+                                const textContent =
+                                  document.createElement("div");
+
+                                const div = element?.appendChild(box);
+                                div?.classList.add("active-box");
+
+                                box.style.width = `${
+                                  40 * res.length + 2 * res.length - 2
+                                }px`;
+
+                                const content = box.appendChild(textContent);
+
+                                if (tableIdx <= 2) {
+                                  content.classList.add("overlay-bottom");
+                                } else {
+                                  content.classList.add("overlay-top");
+                                }
+                                content.addEventListener("click", () => {
+                                  router.push(
+                                    `${pathname}/${table.unitId}-${table.contractDates[findIndex].startedAt}`
+                                  );
+                                });
+
+                                content.innerHTML = `<div><strong>${
+                                  table.unitId
+                                } ${table.unitName}</strong><p>${dayjs(
+                                  res[0]
+                                ).format("YY.MM.DD")} ~ ${dayjs(
+                                  res[res.length - 1]
+                                ).format("YY.MM.DD")}</p><strong>${
+                                  table.name
+                                }<strong></div><button id="detail-btn">상세</button>`;
+                              }
+
                               const element = document.getElementById(
                                 `${item}-${tableIdx}`
                               );
-                              const box = document.createElement("div");
-                              const textContent = document.createElement("div");
-
-                              const div = element?.appendChild(box);
-                              div?.classList.add("active-box");
-
-                              box.style.width = `${
-                                40 * res.length + 2 * res.length - 2
-                              }px`;
-
-                              const title = box.appendChild(textContent);
-
-                              if (tableIdx <= 2) {
-                                title.classList.add("overlay-bottom");
-                              } else {
-                                title.classList.add("overlay-top");
-                              }
-
-                              title.innerHTML = `<div><strong>${table.unitId} ${
-                                table.unitName
-                              }</strong><p>${dayjs(res[0]).format(
-                                "YY.MM.DD"
-                              )} ~ ${dayjs(res[res.length - 1]).format(
-                                "YY.MM.DD"
-                              )}</p><strong>${
-                                table.name
-                              }<strong></div><button>상세</button>`;
-                            }
-
-                            const element = document.getElementById(
-                              `${item}-${tableIdx}`
-                            );
-                            element!.classList.add("active-contract");
-                          });
+                              element?.classList.add("active-contract");
+                            });
+                          }
                         }
-                      }
-                    }}
-                    // onMouseOver={() =>
-                    //   console.log(table.unitName, date, isIclude, temp)
-                    // }
-                  >
-                    {dayjs(date).format("MM.DD")}
-                    {/* {isIclude ? "true" : "false"} */}
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
+                      }}
+                    >
+                      <div
+                        className="col-hover-box"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          removeHoverBox("col");
+                        }}
+                      >
+                        <div className="date-tooltip">
+                          {dayjs(date).format("YY.MM.DD")}
+                        </div>
+                      </div>
+                      {rowIdx === 0 ? (
+                        <div
+                          className="row-hover-box"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            removeHoverBox("row");
+                          }}
+                        />
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+      <p>rowKey:{currentRowIdx}</p>
+    </>
   );
 }
 
-export default Calendar;
+export default memo(Calendar);
--- src/app/utils/dummy.ts
+++ src/app/utils/dummy.ts
@@ -1,6 +1,14 @@
+export type tableType = {
+  unitId: string;
+  unitName: string;
+  name: string;
+  contractDates: { startedAt: string; endedAt: string }[];
+};
+
 export const tableData = [
   {
-    unitId: "서초393",
+    id: 1,
+    unitId: "Seocho393",
     unitName: "311호",
     name: "Antonio shumago",
     contractDates: [
@@ -9,13 +17,19 @@ export const tableData = [
         endedAt: "2024-08-30",
       },
       {
+        startedAt: "2024-09-01",
+        endedAt: "2024-09-03",
+      },
+
+      {
         startedAt: "2024-09-05",
         endedAt: "2024-09-12",
       },
     ],
   },
   {
-    unitId: "용산241",
+    id: 2,
+    unitId: "Yongsan241",
     unitName: "301호",
     name: "Antonio shumago",
     contractDates: [
@@ -26,7 +40,8 @@ export const tableData = [
     ],
   },
   {
-    unitId: "용산242",
+    id: 3,
+    unitId: "Yongsan242",
     unitName: "312호",
     name: "Antonio shumago",
     contractDates: [
@@ -41,7 +56,8 @@ export const tableData = [
     ],
   },
   {
-    unitId: "용산243",
+    id: 4,
+    unitId: "Yongsan243",
     unitName: "313호",
     name: "Antonio shumago",
     contractDates: [
@@ -57,7 +73,8 @@ export const tableData = [
     ],
   },
   {
-    unitId: "용산244",
+    id: 5,
+    unitId: "Yongsan244",
     unitName: "314호",
     name: "Antonio shumago",
     contractDates: [
@@ -68,7 +85,8 @@ export const tableData = [
     ],
   },
   {
-    unitId: "용산245",
+    id: 6,
+    unitId: "Yongsan245",
     unitName: "315호",
     name: "Antonio shumago",
     contractDates: [
@@ -76,6 +94,188 @@ export const tableData = [
         startedAt: "2024-09-08",
         endedAt: "2024-09-24",
       },
+    ],
+  },
+
+  {
+    id: 7,
+    unitId: "Seocho394",
+    unitName: "311호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-08-28",
+        endedAt: "2024-08-30",
+      },
+      {
+        startedAt: "2024-09-01",
+        endedAt: "2024-09-03",
+      },
+
+      {
+        startedAt: "2024-09-05",
+        endedAt: "2024-09-12",
+      },
+    ],
+  },
+  {
+    id: 8,
+    unitId: "Yongsan241",
+    unitName: "301호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-09-01",
+        endedAt: "2024-09-28",
+      },
+    ],
+  },
+  {
+    id: 9,
+    unitId: "Yongsan242",
+    unitName: "312호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-09-02",
+        endedAt: "2024-09-05",
+      },
+      {
+        startedAt: "2024-09-13",
+        endedAt: "2024-09-18",
+      },
+    ],
+  },
+  {
+    id: 10,
+    unitId: "Yongsan243",
+    unitName: "313호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-09-15",
+        endedAt: "2024-09-30",
+      },
+
+      {
+        startedAt: "2024-08-30",
+        endedAt: "2024-09-04",
+      },
+    ],
+  },
+  {
+    id: 11,
+    unitId: "Yongsan244",
+    unitName: "314호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-09-01",
+        endedAt: "2024-09-12",
+      },
+    ],
+  },
+  {
+    id: 12,
+    unitId: "Yongsan245",
+    unitName: "315호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-09-08",
+        endedAt: "2024-09-24",
+      },
+    ],
+  },
+
+  {
+    id: 13,
+    unitId: "Seocho393",
+    unitName: "311호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-08-28",
+        endedAt: "2024-08-30",
+      },
+      {
+        startedAt: "2024-09-01",
+        endedAt: "2024-09-03",
+      },
+
+      {
+        startedAt: "2024-09-05",
+        endedAt: "2024-09-12",
+      },
+    ],
+  },
+  {
+    id: 14,
+    unitId: "Yongsan241",
+    unitName: "301호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-09-01",
+        endedAt: "2024-09-28",
+      },
+    ],
+  },
+  {
+    id: 15,
+    unitId: "Yongsan242",
+    unitName: "312호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-09-02",
+        endedAt: "2024-09-05",
+      },
+      {
+        startedAt: "2024-09-13",
+        endedAt: "2024-09-18",
+      },
+    ],
+  },
+  {
+    id: 16,
+    unitId: "Yongsan243",
+    unitName: "313호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-09-15",
+        endedAt: "2024-09-30",
+      },
+
+      {
+        startedAt: "2024-08-30",
+        endedAt: "2024-09-04",
+      },
+    ],
+  },
+  {
+    id: 17,
+    unitId: "Yongsan244",
+    unitName: "314호",
+    name: "Antonio shumago",
+    contractDates: [
+      {
+        startedAt: "2024-09-01",
+        endedAt: "2024-09-12",
+      },
+    ],
+  },
+  {
+    id: 18,
+    unitId: "Yongsan245",
+    unitName: "315호",
+    name: "Antonio shumago",
+    contractDates: [
+      // {
+      //   startedAt: "2024-09-08",
+      //   endedAt: "2024-09-24",
+      // },
     ],
   },
 ];
